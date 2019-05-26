9d17fe8f81b1130a09bc7c55e7c5d90d41b1603c
xiaoliwang2016 <846590046@qq.com>
2019-05-26 23:37:46 +0800

    2019/0/26 23:37

--- controller/praise.js
+++ controller/praise.js
@@ -6,8 +6,28 @@ class Praise{
 
     }
 
-    add(){
+    /**
+     * 新增点赞
+     */
+    add(req, res, next){
+        req.checkBody('union_id', 'union_id can not be empty').exists()
+        req.checkBody('pic_id', 'pic_id can not be empty').exists()
+        var errors = req.validationErrors()
+        if(errors){
+            return res.status(422).json(errors)
+        }
+        var key = 'praise_' + req.body.pic_id.slice(0, req.body.pic_id.indexOf('.'))
+        console.log(key);
+        console.log(req.body.union_id);
+        console.log(redis.sismember(key, req.body.union_id))
         
+        // if(){
+        //     return res.status(201).end('您已经点赞')
+        // }
+        // redis.sadd(key, req.body.union_id)
+        return res.status(201).end('点赞成功')
     }
 
 }
+
+module.exports = new Praise()
--- routes/image.js
+++ routes/image.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var router = express.Router()
+
 var Image = require('../controller/image')
+var Praise = require('../controller/praise')
 
 router.post('/upload',Image.init, Image.info, Image.thumb, Image.watermark, Image.save)
 router.get('/list', Image.getViaActivityId)
+router.post('/praise', Praise.add)
 
   
 module.exports = router;
